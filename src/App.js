7230acc98fff9b38f7414f53e9bdbf7fd753df6e
Aktan-web <idinex@gk.ru>
2021-08-05 18:24:01 +0600

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ContentLoader from "react-content-loader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [file, setFile] = React.useState()
+    const [preview, setPreview] = React.useState()
+    const inputRef = React.useRef()
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        inputRef.current.click()
+    }
+    const handleChange = (event) => {
+        const file = event.target.files[0]
+        if(file && file.type.substr(0, 5) === "image") {
+            setFile(file)
+        } else {
+            setFile(null)
+        }
+    }
+    React.useEffect(() => {
+        if(file) {
+            const reader = new FileReader()
+            reader.onloadend = event => {
+                setPreview(reader.result)
+            }
+            reader.readAsDataURL(file)
+        } else {
+            setPreview(null)
+        }
+    }, [file])
+  return(
+      <div>
+          {preview ? (<img style={{objectFit: 'cover'}} src={preview} onClick={() => setFile(null)}/>) :
+              (
+                  <div className={"circle"} onClick={handleClick}>
+                      <i className="fas fa-images gallery"></i>
+                      <i className={"fas fa-times remove"}></i>
+                  </div>
+              )
+          }
+        <input type={"file"} ref={inputRef} onChange={handleChange} />
+      </div>
+  )
 }
 
-export default App;
+export default App
